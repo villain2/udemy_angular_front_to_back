8ec9bc3e00ccb9ce668b05d7cffbba32ceb182b9
K.C. Hunter <djvillain@hotmail.com>
2019-11-24 08:00:03 -0500

    Completed Lesson 28 ngModel and 2 Way Data Binding.

--- src/app/components/users/users.component.ts
+++ src/app/components/users/users.component.ts
@@ -8,10 +8,20 @@ import { User } from '../../models/User';
   styleUrls: ['./users.component.css']
 })
 export class UsersComponent implements OnInit {
+  user: User = {
+    firstName: '',
+    lastName: '',
+    age: null,
+    address: {
+      street: '',
+      city: '',
+      state: ''
+    }
+  };
   users: User[];
   showExtended: boolean = true;
   loaded: boolean = false;
-  enableAdd: boolean = true;
+  enableAdd: boolean = false;
   showUserForm: boolean = false;
 
   constructor(private dataService: DataService) {
@@ -64,8 +74,21 @@ export class UsersComponent implements OnInit {
     this.loaded = true;
   }
 
-  addUser(user: User) {
-    this.users.push(user);
+  addUser() {
+    this.user.isActive = true;
+    this.user.registered = new Date();
+    this.users.unshift(this.user);
+    //clear form
+    this.user = {
+      firstName: '',
+      lastName: '',
+      age: null,
+      address: {
+        street: '',
+        city: '',
+        state: ''
+      }
+    }
   }
 
   toggleHide(user: User) {
